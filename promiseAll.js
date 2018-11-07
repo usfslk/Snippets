f0bd67080b2491a64c96ab82be2cc41b2f3e5dd7
Youssef Selkani <Ynakles@gmail.com>
2018-11-07 23:13:16 +0000

    Update promiseAll.js

--- promiseAll.js
+++ promiseAll.js
@@ -3,3 +3,5 @@ Promise.all([promise1,promise2,promise3]).then(function(result) {
 }).catch(function(error){
     console.log(error) 
 });
+
+// Promise.all() method call rejects when one of the Promises that it is processing rejects
