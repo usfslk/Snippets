ca77311687df0beb62cb9170c55c6aeff738abb5
Youssef Selkani <Ynakles@gmail.com>
2018-11-07 23:40:53 +0000

    Create responseStatus.js

---
+++ responseStatus.js
@@ -0,0 +1,17 @@
+fetch("https://jsonplaceholder.typicode.com/bad_url/1")
+.then(function(result){ //contains a Response object
+    console.log(result);
+    if(result.ok){ //returns true if the Response status is within 200-299
+        return result.text(); 
+    }
+    else{ //if the fetch request had problems
+        console.log(result.status) //logs 404
+        return Promise.reject(result.status); //returns a rejected promise if the fetch request had problems
+    }
+})
+.then(function(result){
+    console.log(result); //doesn't occur 
+})
+.catch(function(err){
+    console.log("Error: " +  err); //logs "Error: 404"
+})
